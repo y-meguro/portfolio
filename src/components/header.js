1bad4846920687472938af08fea1e879e4492b60
Yohei Meguro <y.meguro.2008@gmail.com>
2020-07-15 00:58:41 +0900

    add elements in header

--- src/components/header.js
+++ src/components/header.js
@@ -6,24 +6,38 @@ import profile from '../assets/images/profile.jpg'
 const Header = () => {
   return (
     <header className="header">
-      <div className="profile">
+      <div>
         <img src={profile} alt="" />
         <h1>Yohei Meguro</h1>
+        <h2>Software Engineer</h2>
         <ul>
           <li>
-            <a href="https://twitter.com/yohei_meguro" className="icon">
+            <a href="https://twitter.com/yohei_meguro" className="icon" target="_blank">
               <span area-hidden="true" className="fa fa-twitter"></span>
             </a>
           </li>
           <li>
-            <a href="https://github.com/y-meguro" className="icon">
+            <a href="https://github.com/y-meguro" className="icon" target="_blank">
               <span area-hidden="true" className="fa fa-github"></span>
             </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/yoheimeguro/" className="icon">
+            <a href="https://www.linkedin.com/in/yoheimeguro/" className="icon" target="_blank">
               <span area-hidden="true" className="fa fa-linkedin"></span>
             </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Resume</a>
+          </li>
+          <li>
+            <a href="https://blog.ymeguro.com/" target="_blank">Blog</a>
           </li>
         </ul>
       </div>
