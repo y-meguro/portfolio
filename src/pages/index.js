4e1362a7059a74ddc5d5623d0841104531a23d41
Yohei Meguro <y.meguro.2008@gmail.com>
2020-07-17 18:23:52 +0900

    update section title style

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,25 +7,25 @@ const IndexPage = () => {
       <div className="main-wrapper">
         <div className="main-section">
           <section>
-            <h1>About Me</h1>
+            <h1 className="section-title">About Me</h1>
             <p>
               こんにちは。ソフトウェアエンジニアをしています。
             </p>
           </section>
           <section>
-            <h1>Experience</h1>
+            <h1 className="section-title">Experience</h1>
             <p>
               hogehoge
             </p>
           </section>
           <section>
-            <h1>Latest Blog Posts</h1>
+            <h1 className="section-title">Latest Blog Posts</h1>
             <p>
               hogehoge
             </p>
           </section>
           <section>
-            <h1>Links</h1>
+            <h1 className="section-title">Links</h1>
             <p>
               hogehoge
             </p>
