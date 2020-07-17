495b10cba929b8233b2d70096258cfe7d7cb8c9d
Yohei Meguro <y.meguro.2008@gmail.com>
2020-07-17 16:45:08 +0900

    fix ESLintError

--- src/components/header.js
+++ src/components/header.js
@@ -8,22 +8,22 @@ const Header = () => {
     <header className="header text-center">
       <div className="navbar">
         <div className="profile-section">
-          <img className="profile-image rounded-circle" src={profile} alt="image" />
+          <img className="profile-image rounded-circle" src={profile} alt="profile" />
           <h1 className="name">Yohei Meguro</h1>
           <h2 className="title">Software Engineer</h2>
           <ul className="social-icons list-inline">
             <li className="list-inline-item">
-              <a href="https://twitter.com/yohei_meguro" target="_blank">
+              <a href="https://twitter.com/yohei_meguro" target="_blank" rel="noopener noreferrer">
                 <span area-hidden="true" className="fa fa-twitter"></span>
               </a>
             </li>
             <li className="list-inline-item">
-              <a href="https://github.com/y-meguro" target="_blank">
+              <a href="https://github.com/y-meguro" target="_blank" rel="noopener noreferrer">
                 <span area-hidden="true" className="fa fa-github"></span>
               </a>
             </li>
             <li className="list-inline-item">
-              <a href="https://www.linkedin.com/in/yoheimeguro/" target="_blank">
+              <a href="https://www.linkedin.com/in/yoheimeguro/" target="_blank" rel="noopener noreferrer">
                 <span area-hidden="true" className="fa fa-linkedin"></span>
               </a>
             </li>
@@ -38,7 +38,7 @@ const Header = () => {
               <a href="/" className="page">Resume</a>
             </li>
             <li>
-              <a href="https://blog.ymeguro.com/" target="_blank" className="page">Blog</a>
+              <a href="https://blog.ymeguro.com/" target="_blank" className="page" rel="noopener noreferrer">Blog</a>
             </li>
           </ul>
         </div>
