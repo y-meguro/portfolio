28c6279b7fa42856656ff0bd4af7efb9e25728b0
Yohei Meguro <y.meguro.2008@gmail.com>
2020-07-17 17:10:28 +0900

    add sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,34 @@ import Layout from "../components/layout";
 const IndexPage = () => {
   return (
     <Layout>
-      <div>Hello world!</div>
+      <div className="main-wrapper">
+        <div className="main-section">
+          <section>
+            <h1>About Me</h1>
+            <p>
+              こんにちは。ソフトウェアエンジニアをしています。
+            </p>
+          </section>
+          <section>
+            <h1>Experience</h1>
+            <p>
+              hogehoge
+            </p>
+          </section>
+          <section>
+            <h1>Latest Blog Posts</h1>
+            <p>
+              hogehoge
+            </p>
+          </section>
+          <section>
+            <h1>Links</h1>
+            <p>
+              hogehoge
+            </p>
+          </section>
+        </div>
+      </div>
     </Layout>
   )
 };
