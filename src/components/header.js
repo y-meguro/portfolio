c57e8b6fad778d0e8cfb379dbe964d25e22cb169
Yohei Meguro <y.meguro.2008@gmail.com>
2020-07-17 20:36:42 +0900

    add nabvar.scss

--- src/components/header.js
+++ src/components/header.js
@@ -1,46 +1,45 @@
 import React from "react";
 
 import profile from '../assets/images/profile.jpg'
+import "../assets/scss/navbar.scss"
 
 const Header = () => {
   return (
-    <header className="header text-center">
-      <div className="navbar">
-        <div className="profile-section">
-          <img className="profile-image rounded-circle" src={profile} alt="profile" />
-          <h1 className="name">Yohei Meguro</h1>
-          <h2 className="title">Software Engineer</h2>
-          <ul className="social-icons list-inline">
-            <li className="list-inline-item">
-              <a href="https://twitter.com/yohei_meguro" target="_blank" rel="noopener noreferrer">
-                <span area-hidden="true" className="fa fa-twitter"></span>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="https://github.com/y-meguro" target="_blank" rel="noopener noreferrer">
-                <span area-hidden="true" className="fa fa-github"></span>
-              </a>
-            </li>
-            <li className="list-inline-item">
-              <a href="https://www.linkedin.com/in/yoheimeguro/" target="_blank" rel="noopener noreferrer">
-                <span area-hidden="true" className="fa fa-linkedin"></span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="page-section">
-          <ul className="pages">
-            <li>
-              <a href="/" className="page">About</a>
-            </li>
-            <li>
-              <a href="/" className="page">Resume</a>
-            </li>
-            <li>
-              <a href="https://blog.ymeguro.com/" target="_blank" className="page" rel="noopener noreferrer">Blog</a>
-            </li>
-          </ul>
-        </div>
+    <header className="navbar">
+      <div className="navbar__section">
+        <img className="navbar__profile" src={profile} alt="profile" />
+        <h1 className="navbar__name">Yohei Meguro</h1>
+        <h2 className="navbar__title">Software Engineer</h2>
+        <ul className="navbar__social-icons">
+          <li className="navbar__social-icon">
+            <a href="https://twitter.com/yohei_meguro" target="_blank" rel="noopener noreferrer">
+              <span area-hidden="true" className="fa fa-twitter"></span>
+            </a>
+          </li>
+          <li className="navbar__social-icon">
+            <a href="https://github.com/y-meguro" target="_blank" rel="noopener noreferrer">
+              <span area-hidden="true" className="fa fa-github"></span>
+            </a>
+          </li>
+          <li className="navbar__social-icon">
+            <a href="https://www.linkedin.com/in/yoheimeguro/" target="_blank" rel="noopener noreferrer">
+              <span area-hidden="true" className="fa fa-linkedin"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar__section">
+        <ul className="navbar__links">
+          <li>
+            <a href="/" className="navbar__link">About</a>
+          </li>
+          <li>
+            <a href="/" className="navbar__link">Resume</a>
+          </li>
+          <li>
+            <a href="https://blog.ymeguro.com/" target="_blank" className="navbar__link" rel="noopener noreferrer">Blog</a>
+          </li>
+        </ul>
       </div>
     </header>
   );
